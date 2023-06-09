005cceb7fa1d0ab3ec988a2b9b71513e3d5fda94
hmcclew <122323895+hmcclew@users.noreply.github.com>
2023-06-09 18:43:40 -0400

    updated style

--- src/test/java/cs3500/pa04/client/controller/ProxyControllerTest.java
+++ src/test/java/cs3500/pa04/client/controller/ProxyControllerTest.java
@@ -1,30 +1,25 @@
 package cs3500.pa04.client.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.JsonParser;
-import cs3500.pa04.json.MessageJson;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import cs3500.pa04.client.model.player.Player;
-
-import java.io.InputStream;
-import java.io.OutputStream;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.net.Socket;
-
-import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
 
 class ProxyControllerTest {
 
   @Test
   public void testRun() throws IOException {
-    String serverMessage = "{\"method-name\":\"end-game\",\"arguments\":{\"result\":\"WIN\"," +
+    String serverMessage = "{\"method-name\":\"end-game\",\"arguments\":{\"result\":\"WIN\","
+        +
         "\"reason\":\"You win\"}}";
     ByteArrayInputStream serverInputStream = new ByteArrayInputStream(serverMessage.getBytes());
     ByteArrayOutputStream serverOutputStream = new ByteArrayOutputStream();
@@ -41,6 +36,6 @@ class ProxyControllerTest {
     verify(mockSocket, times(1)).getInputStream();
     verify(mockSocket, times(1)).getOutputStream();
     verify(mockPlayer, times(1)).endGame(any(), anyString());
-    serverInputStream.close();
+    mockSocket.close();
   }
 }
